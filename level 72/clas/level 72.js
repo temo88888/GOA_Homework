f97e89c3432819be99d29df3eac9e2e6054b12dc
temo88888 <Temokhveladze@gmail.com>
2025-01-13 14:46:53 +0400

    commit

---
+++ level 72/clas/level 72.js
@@ -0,0 +1,23 @@
+let age  = Number(prompt("age?"))
+if (age >= 5) {
+    console.log("შენ ხარ ბავშვი")
+}  else if (age >= 80) {
+    "დაბერდი ძმაო'"
+}
+else if(age >= 12) {
+    console.log("შენ ხარ თინეიჯერი/მოზარდი")
+}else (age <= 18) {
+    ("შენ ხარ სრულწოვანი")
+}
+
+
+
+
+// N2
+
+let ricxvi = Number(prompt("number"))
+if (ricxvi == 2,4,6,8,10,12,14,16,18,20,22,24,26,28,30) {
+    console.log("ლუწია")
+} else (ricxvi === 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29) {
+    console.log("კენტი")
+}
